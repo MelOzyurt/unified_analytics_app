7060fe7caf202516edb2a4c23a3abd372062a2c7
MelOzyurt <mozyurt2000@gmail.com>
2025-07-13 23:28:16 +0100

    Create __init__.py

---
+++ analysis/__init__.py
@@ -0,0 +1,4 @@
+from .analyze_data import analyze_data_ui
+from .analyze_data_ai import analyze_data_ai_ui
+from .analyze_feedback import analyze_feedback_ui
+from .chat_with_doc import chat_with_doc_ui
